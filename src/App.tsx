b0ceb5d155139e53fb80e8634cc912e969a2e364
YooKyuBong <rbqhd9709@naver.com>
2025-02-24 14:13:16 +0900

    Initialize project using Create React App

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+// import { ClassComponent } from './component/component';
+// import { FunctionalComponent } from './component/component';
+import CurlyBraces from './component/CurlyBraces';
+
+function App() {
+  return (
+    <div>
+     {/* <ClassComponent/>
+     <FunctionalComponent/> */}
+     <CurlyBraces/>
+    </div>
+  );
+}
+
+export default App;
