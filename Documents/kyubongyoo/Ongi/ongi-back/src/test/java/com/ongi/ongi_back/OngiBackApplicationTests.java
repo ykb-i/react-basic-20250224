5b6f80af930e85fa3a8a0583ef69049766eb683f
YooKyuBong <58059368+ykb-i@users.noreply.github.com>
2025-04-10 16:21:16 +0900

    delete trash folder

---
+++ Documents/kyubongyoo/Ongi/ongi-back/src/test/java/com/ongi/ongi_back/OngiBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ongi.ongi_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OngiBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
